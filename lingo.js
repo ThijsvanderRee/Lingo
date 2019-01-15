fd3fed582e1c3bea3d1986c8129270e8825dfa75
Unknown <99053846@mydavinci.nl>
2019-01-15 13:17:32 +0100

    Added

--- lingo.js
+++ lingo.js
@@ -1,5 +1,11 @@
 var random = words[Math.floor(Math.random() * words.length)];
+
 var letter1 = random.charAt(0).split();
+var letter2 = random.charAt(1).split();
+var letter3 = random.charAt(2).split();
+var letter4 = random.charAt(3).split();
+var letter5 = random.charAt(4).split();
+
 console.log(random);
 
 function createboxes() {
@@ -13,8 +19,6 @@ function createboxes() {
 	
 }
 
-var box1 = document.getElementById("box1");
-
 var input = document.getElementById("inputbox");
 
 var guess = [];
@@ -26,9 +30,19 @@ function checkword() {
 	console.log(guess);
 	console.log(guess[0]);
 	console.log(document.getElementById("box1").value);
-	if (input[0] == document.getElementById("box1").value) {
-		document.getElementById("box1").style.backgroundColor = "groen";
-	} else {
-		document.getElementById("box1").style.backgroundColor = "red";
+	
+	document.getElementById("box1").value = input2[0];
+	document.getElementById("box2").value = input2[1];
+	document.getElementById("box3").value = input2[2];
+	document.getElementById("box4").value = input2[3];
+	document.getElementById("box5").value = input2[4];
+
+	for (i = 1; i < 6; i++) {
+		var x = 0; x < 5; x++;
+		if (input[x] == document.getElementById("box"+[i]).value) {
+			document.getElementById("box"+[i]).style.backgroundColor = "lime";
+		} else {
+			document.getElementById("box"+[i]).style.backgroundColor = "red";
+		}
 	}
 }
